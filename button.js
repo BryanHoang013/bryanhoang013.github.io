a7a6f9aed9dc02e045b7143d4c4079446c7c8179
BryanHoang013 <152670546+BryanHoang013@users.noreply.github.com>
2023-12-01 22:06:33 -0500

    Add files via upload

---
+++ button.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function Button() {
+  return (
+    <div className="App">
+     <button>Button 1</button>
+     <button>Button 2</button>
+     <button>Button 3</button>
+     <button>Button 4</button>
+     <button>Button 5</button>
+    </div>
+  );
+}
+
+
+export default Button;
